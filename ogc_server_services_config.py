a7da3ee2fd213167e1097985faa8c31c366792c7
kamijawa <kamijawa@mail.com>
2014-06-30 17:20:57 +0800

    add

---
+++ ogc_server_services_config.py
@@ -0,0 +1,10 @@
+# -*- coding: gb18030 -*-
+NAME = 'ogc_server_%s'
+DISPLAY_NAME = 'OGC Server - %s'
+MODULE_NAME = 'ogc_server'
+CLASS_NAME = 'Win32ServiceHandler'
+DESCRIPTION = 'OGC通用WEBGIS服务器'
+AUTO_START = True
+SESSION_CHANGES = False
+
+
