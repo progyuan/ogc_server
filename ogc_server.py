a52416b44b3da593598ea9a96e3c40dc15ce675e
kamijawa <kamijawa@mail.com>
2015-04-15 15:59:11 +0800

    mod

--- ogc_server.py
+++ ogc_server.py
@@ -273,6 +273,8 @@ def handle_static(environ, aUrl):
         if gConfig['mime_type'].has_key(ext):
             if 'image/' in gConfig['mime_type'][ext]:
                 isBin = True
+            if '/octet-stream' in gConfig['mime_type'][ext]:
+                isBin = True
             contenttype = gConfig['mime_type'][ext]
             if ext == '.js':
                 if not os.path.exists(p):
