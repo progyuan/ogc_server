629414e0421ee8374061686af94d803fdd478559
kamijawa <kamijawa@mail.com>
2015-06-27 18:58:22 +0800

    mod

--- db_util.py
+++ db_util.py
@@ -10138,7 +10138,7 @@ if __name__=="__main__":
     #test_remove_blank_tile()
     #print(get_heatmap_tile_service_list('yn'))
     #test_import_userinfo()
-    test_import_sysrole(db_name)
+    # test_import_sysrole(db_name)
     #test_auth()
     #test_kml_import()
     #test_delete_anti_bird()
