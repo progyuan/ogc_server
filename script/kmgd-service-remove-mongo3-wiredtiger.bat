a23953adb172195c7832b0c96efdd6dab06e9322
xiejun <xiejun@yncft.cn>
2016-05-06 19:06:50 +0800

    mod

--- script/kmgd-service-remove-mongo3-wiredtiger.bat
+++ script/kmgd-service-remove-mongo3-wiredtiger.bat
@@ -1,2 +1,2 @@
-"H:\Program Files\MongoDB\bin\mongod.exe" --remove --serviceName "Mongo3-wiredtiger-27017"
+"F:\Program Files\MongoDB\Server\3.2\bin\mongod.exe" --remove --serviceName "Mongo3-wiredtiger-27017"
 pause
